f45eb1dda95f786cf8dd9fa98e64357d8de95226
YoonDaewon <ydwinha@naver.com>
2016-04-03 15:54:31 +0900

    깃허브 android용, eclipse 용으로 분리

--- Daum_Map_API/Android_DaumMap_Sample_1.2.15.5/Android_DaumMap_Sample_1.2.15.5/app/src/main/java/net/daum/android/map/openapi/sampleapp/MapApiConst.java
+++
@@ -1,9 +0,0 @@
-package net.daum.android.map.openapi.sampleapp;
-
-/**
- * Created by pjh on 2014. 9. 15..
- */
-public class MapApiConst {
-	// http://developers.daum.net/console
-    public static final String DAUM_MAPS_ANDROID_APP_API_KEY = "8d1c05f70da4a6849791f1f548f6368c";
-}
--- Daum_Map_API/Android_DaumMap_Sample_1.2.15.5/Android_DaumMap_Sample_1.2.15.5/app/src/main/java/net/daum/android/map/openapi/search/OnFinishSearchListener.java
+++
@@ -1,8 +0,0 @@
-package net.daum.android.map.openapi.search;
-
-import java.util.List;
-
-public interface OnFinishSearchListener {
-	public void onSuccess(List<Item> itemList);
-	public void onFail();
-}
