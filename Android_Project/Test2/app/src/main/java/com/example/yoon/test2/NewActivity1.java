cf0787a1c9db1437efbf0b17bef4606e14ffeea6
YoonDaewon <ydwinha@naver.com>
2016-04-11 17:54:14 +0900

    Android - 채팅창 추가 및 Toast로 출력

--- Android_Project/Test2/app/src/main/java/com/example/yoon/test2/NewActivity1.java
+++ Android_Project/Test2/app/src/main/java/com/example/yoon/test2/NewActivity1.java
@@ -3,6 +3,8 @@ package com.example.yoon.test2;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class NewActivity1 extends AppCompatActivity {
@@ -11,6 +13,14 @@ public class NewActivity1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new1);
+        Button btn = (Button)findViewById(R.id.btn);
+        btn.setOnClickListener(new Button.OnClickListener(){
+            public void onClick(View v){
+                EditText edit = (EditText) findViewById(R.id.edit);
+                String str = edit.getText().toString();
+                Toast.makeText(NewActivity1.this, str, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     public void onbackButtonClicked(View v){
