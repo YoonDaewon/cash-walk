cdfcf064641c461cabd06e95f313cd055de4dc39
YoonDaewon <ydwinha@naver.com>
2016-04-18 00:41:09 +0900

    Android - GPS를 이용한 맵 출력 재도전 실패. 권환 에러 해결 못함

--- Android_Project/GPS/app/src/main/java/com/example/yoon/gps/MainActivity.java
+++ Android_Project/GPS/app/src/main/java/com/example/yoon/gps/MainActivity.java
@@ -6,19 +6,19 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.location.Location;
-import android.location.LocationManager;
 import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
+import android.view.WindowManager;
+import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.model.LatLng;
 
 public class MainActivity extends AppCompatActivity {
@@ -37,6 +37,89 @@ public class MainActivity extends AppCompatActivity {
 
         startLocationService();
     }
+
+    public void onResume(){
+        super.onResume();
+
+        map.setMyLocationEnabled(true);
+        if(mCompassEnabled) {
+            mSensorManager.registerListener(mListener, mSensorManager, getDefaultSensor(Sensor.TYPE_ORIENTATION), SensorManager.SENSOR_DELAY_UI);
+        }
+    }
+
+    public void onPause(){
+        super.onPause();
+
+        map.setMyLocationEnabled(false);
+        if(mCompassEnabled){
+            mSensorManager.unregisterListener(mListener);
+        }
+    }
+
+    private void startLocationService(){
+        LocationManager manager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        GPSListener gpsListener = new GPSListener;
+        long minTime = 10000;
+        float minDistance = 0;
+
+        manager.requestLocationUpdates(
+                LocationManager.GPS_PROVIDER,
+                minTime,
+                minDistance,
+                gpsListener);
+
+        Toast.makeText(getApplicationContext(),"위치 확인 시작함. 로그를 확인",Toast.LENGTH_SHORT).show();
+
+    }
+
+    private class GPSListener implements LocationListener{
+
+        public void onLocationChanged(Location location){
+            Double latitude = location.getLatitude();
+            Double longitude = location.getLongitude();
+
+            String msg = "Latitude : " + latitude + "\nLongitude :" + longitude;
+            Log.i("GPSLocationService", msg);
+
+            showCurrentLocation(latitude,longitude);
+        }
+
+        public void onProviderDisabled(String provider){
+
+        }
+
+        public  void onProviderEnabled(String provider){
+
+        }
+        public void onStatusChanged(String provide, int status, Bundle extras){
+
+        }
+    }
+
+    private  void showCurrentLocation(Double latitude, Double longitude){
+        LatLng curPoint = new LatLng(latitude,longitude);
+
+        map.animateCamera(CameraUpdateFactory.newLatLngZoom(curPoint,15));
+
+        map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+    }
+
+    private  final SensorEventListener mListener = new SensorEventListener() {
+        private int iOrientation = -1;
+
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            if(iOrientation<0){
+                iOrientation = ((WindowManager) getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay().getRotation();
+            }
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+        }
+    };
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
