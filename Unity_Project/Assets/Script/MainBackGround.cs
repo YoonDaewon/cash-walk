d0a37f3e01f096b3848755730f27d8574d250d81
poket1121 <poket1121@naver.com>
2016-04-28 20:24:56 +0900

    Unity - 메인화면 스크롤링 제작과정

---
+++ Unity_Project/Assets/Script/MainBackGround.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainBackGround : MonoBehaviour {
+
+    public float speed = 0.5f;
+
+    Material mt;
+	// Use this for initialization
+	void Start () {
+        mt = GetComponent<Renderer>().material;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float offsetY = mt.mainTextureOffset.y + (Time.deltaTime * speed);
+        Vector2 modifyOffsetY = new Vector2(0, offsetY);
+        mt.mainTextureOffset = modifyOffsetY;
+	}
+}
