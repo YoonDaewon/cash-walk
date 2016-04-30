986fcab91d3e725d772a30c45a6b3f4a448c4e0b
poket1121 <poket1121@naver.com>
2016-04-30 17:09:52 +0900

    유니티 - 스크롤링 바깥배경 사이즈조절.

--- Android_Project/NMAPTEST/NMapViewer(android)_v2.1.1_open (1)/app/build/generated/source/buildConfig/debug/com/TestMap/BuildConfig.java
+++ Android_Project/NMAPTEST/NMapViewer(android)_v2.1.1_open (1)/app/build/generated/source/buildConfig/debug/com/TestMap/BuildConfig.java
@@ -1,7 +1,7 @@
 /**
  * Automatically generated file. DO NOT MODIFY
  */
-package com.android.application;
+package com.TestMap;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
--- Android_Project/NMAPTEST/NMapViewer(android)_v2.1.1_open (1)/app/build/generated/source/r/debug/com/TestMap/R.java
+++ Android_Project/NMAPTEST/NMapViewer(android)_v2.1.1_open (1)/app/build/generated/source/r/debug/com/TestMap/R.java
@@ -5,7 +5,7 @@
  * should not be modified by hand.
  */
 
-package com.android.application;
+package com.TestMap;
 
 public final class R {
     public static final class attr {
