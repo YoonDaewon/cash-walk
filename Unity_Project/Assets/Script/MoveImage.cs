eee55178e365380af197f8378c16fefcb8620a41
poket1121 <poket1121@naver.com>
2016-05-03 15:32:35 +0900

    유니티 메인씬 스크롤링 제작

---
+++ Unity_Project/Assets/Script/MoveImage.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveImage : MonoBehaviour {
+
+    public GameObject moveImage_1;
+    public GameObject moveImage_2;
+    public float speed;
+
+    Vector3 image_Position;
+
+    // Use this for initialization
+
+    void Start () {
+        Vector3 start_Position = new Vector3(420.0f,228.0f,0.0f);
+        moveImage_1.transform.position = start_Position;
+        start_Position = new Vector3(1620.0f, 228.0f, 0.0f);
+        moveImage_2.transform.position = start_Position;
+        speed = -3.0f;
+        image_Position = new Vector3(speed, 0.0f, 0.0f);
+	}
+	
+	// Update is called once per frame
+    void Update()
+    {
+        moveImage_1.transform.position += image_Position;
+        moveImage_2.transform.position += image_Position;
+        if (moveImage_1.transform.position.x < -780.0f)
+        {
+            moveImage_1.transform.position = new Vector3(1620.0f, 228.0f, 0.0f);
+        }
+        if (moveImage_2.transform.position.x < -780.0f)
+        {
+            moveImage_2.transform.position = new Vector3(1620.0f, 228.0f, 0.0f);
+        }
+	}
+}
