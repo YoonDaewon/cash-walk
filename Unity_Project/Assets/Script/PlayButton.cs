61b3ddf35b3309bdb80b969f194ac3f52b19d015
poket1121 <poket1121@naver.com>
2016-05-07 22:24:21 +0900

    병합 및 브랜치 정리

---
+++ Unity_Project/Assets/Script/PlayButton.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayButton : MonoBehaviour {
+    
+    
+    public void PlayBt(int num)
+    {
+        if (num==1)
+        {
+            Application.LoadLevel("PlayScene");
+        }
+    }
+
+
+	// Use this for initialization
+	void Start () {
+	
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Unity_Project/Assets/Script/StartNextMain.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartNextMain : MonoBehaviour {
+
+    private float TimeLeft = 3.0f;
+
+    public void NextScene()
+    {
+        Application.LoadLevel("MainScene");
+    }
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Time.time > TimeLeft)
+        {
+            NextScene();
+        }
+	
+	}
+}
