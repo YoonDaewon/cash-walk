962b0eb59f2e9b4790b2d2dd911ad6d0eae3cb89
poket1121 <poket1121@naver.com>
2016-05-16 01:26:11 +0900

    Unity project 버튼크기, 스크롤링수정

--- Unity_Project/Assets/Script/MoveImage.cs
+++ Unity_Project/Assets/Script/MoveImage.cs
@@ -8,15 +8,17 @@ public class MoveImage : MonoBehaviour {
     public float speed;
 
     Vector3 image_Position;
-
+    Vector2 start_Position1, start_Position2;
     // Use this for initialization
 
     void Start () {
-        Vector3 start_Position = new Vector3(420.0f,228.0f,0.0f);
-        moveImage_1.transform.position = start_Position;
-        start_Position = new Vector3(1620.0f, 228.0f, 0.0f);
-        moveImage_2.transform.position = start_Position;
-        speed = -3.0f;
+        //Vector3 start_Position = new Vector3(420.0f,228.0f,0.0f);
+        //moveImage_1.transform.position = start_Position;
+        //start_Position = new Vector3(1620.0f, 228.0f, 0.0f);
+        //moveImage_2.transform.position = start_Position;
+        start_Position1 = moveImage_1.transform.position;
+        start_Position2 = moveImage_2.transform.position;
+        speed = -10.0f;
         image_Position = new Vector3(speed, 0.0f, 0.0f);
 	}
 	
@@ -25,13 +27,15 @@ public class MoveImage : MonoBehaviour {
     {
         moveImage_1.transform.position += image_Position;
         moveImage_2.transform.position += image_Position;
-        if (moveImage_1.transform.position.x < -780.0f)
+        if (moveImage_1.transform.position.x < -2400.0f)
         {
-            moveImage_1.transform.position = new Vector3(1620.0f, 228.0f, 0.0f);
+            //moveImage_1.transform.position = new Vector3(1620.0f, 228.0f, 0.0f);
+            moveImage_1.transform.position = start_Position2;
         }
-        if (moveImage_2.transform.position.x < -780.0f)
+        if (moveImage_2.transform.position.x < -2400.0f)
         {
-            moveImage_2.transform.position = new Vector3(1620.0f, 228.0f, 0.0f);
+           // moveImage_2.transform.position = new Vector3(1620.0f, 228.0f, 0.0f);
+            moveImage_2.transform.position = start_Position2;
         }
 	}
 }
