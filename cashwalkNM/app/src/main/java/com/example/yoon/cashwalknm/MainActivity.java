a81641b074161c7d917615a873af0b9989c1ca5e
YoonDaewon <ydwinha@naver.com>
2016-08-08 15:00:33 +0900

    NMAP - 기본 프레임1

--- cashwalkNM/app/src/main/java/com/example/yoon/cashwalknm/MainActivity.java
+++ cashwalkNM/app/src/main/java/com/example/yoon/cashwalknm/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.yoon.cashwalknm;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.widget.Toast;
 
 import com.nhn.android.maps.NMapActivity;
 import com.nhn.android.maps.NMapController;
@@ -13,10 +16,26 @@ import com.nhn.android.maps.nmapmodel.NMapError;
 
 public class MainActivity extends NMapActivity {
 
+    //정의
     private NMapView mMapView;
     private NMapController mMapController;
     private static final String CLIENT_ID = "jLSZMsTP7rso3Qlz80_X";
     private static final String LOG_TAG = "Cash Walk";
+    private static final boolean DEBUG = false;
+    private SharedPreferences mPreferences;
+
+    private static final String KEY_ZOOM_LEVEL = "NMapViewer.zoomLevel";
+    private static final String KEY_CENTER_LONGITUDE = "NMapViewer.centerLongitudeE6";
+    private static final String KEY_CENTER_LATITUDE = "NMapViewer.centerLatitudeE6";
+    private static final String KEY_VIEW_MODE = "NMapViewer.viewMode";
+    private static final String KEY_TRAFFIC_MODE = "NMapViewer.trafficMode";
+    private static final String KEY_BICYCLE_MODE = "NMapViewer.bicycleMode";
+
+    private static final NGeoPoint NMAP_LOCATION_DEFAULT = new NGeoPoint(126.978371, 37.5666091);
+    private static final int NMAP_ZOOMLEVEL_DEFAULT = 11;
+    private static final int NMAP_VIEW_MODE_DEFAULT = NMapView.VIEW_MODE_VECTOR;
+    private static final boolean NMAP_TRAFFIC_MODE_DEFAULT = false;
+    private static final boolean NMAP_BICYCLE_MODE_DEFAULT = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -76,4 +95,107 @@ public class MainActivity extends NMapActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    /* MapView State Change Listener*/
+    private final NMapView.OnMapStateChangeListener onMapViewStateChangeListener = new NMapView.OnMapStateChangeListener() {
+
+        @Override
+        public void onMapInitHandler(NMapView mapView, NMapError errorInfo) {
+
+            if (errorInfo == null) { // success
+                // restore map view state such as map center position and zoom level.
+                restoreInstanceState();
+
+            } else { // fail
+                Log.e(LOG_TAG, "onFailedToInitializeWithError: " + errorInfo.toString());
+
+                Toast.makeText(MainActivity.this, errorInfo.toString(), Toast.LENGTH_LONG).show();
+            }
+        }
+
+        @Override
+        public void onAnimationStateChange(NMapView mapView, int animType, int animState) {
+            if (DEBUG) {
+                Log.i(LOG_TAG, "onAnimationStateChange: animType=" + animType + ", animState=" + animState);
+            }
+        }
+
+        @Override
+        public void onMapCenterChange(NMapView mapView, NGeoPoint center) {
+            if (DEBUG) {
+                Log.i(LOG_TAG, "onMapCenterChange: center=" + center.toString());
+            }
+        }
+
+        @Override
+        public void onZoomLevelChange(NMapView mapView, int level) {
+            if (DEBUG) {
+                Log.i(LOG_TAG, "onZoomLevelChange: level=" + level);
+            }
+        }
+
+        @Override
+        public void onMapCenterChangeFine(NMapView mapView) {
+
+        }
+    };
+
+    /* Local Functions */
+    private static boolean mIsMapEnlared = false;
+
+    //  restore 인스턴스 정의
+    private void restoreInstanceState() {
+        mPreferences = getPreferences(MODE_PRIVATE);
+
+        int longitudeE6 = mPreferences.getInt(KEY_CENTER_LONGITUDE, NMAP_LOCATION_DEFAULT.getLongitudeE6());
+        int latitudeE6 = mPreferences.getInt(KEY_CENTER_LATITUDE, NMAP_LOCATION_DEFAULT.getLatitudeE6());
+        int level = mPreferences.getInt(KEY_ZOOM_LEVEL, NMAP_ZOOMLEVEL_DEFAULT);
+        int viewMode = mPreferences.getInt(KEY_VIEW_MODE, NMAP_VIEW_MODE_DEFAULT);
+        boolean trafficMode = mPreferences.getBoolean(KEY_TRAFFIC_MODE, NMAP_TRAFFIC_MODE_DEFAULT);
+        boolean bicycleMode = mPreferences.getBoolean(KEY_BICYCLE_MODE, NMAP_BICYCLE_MODE_DEFAULT);
+
+        mMapController.setMapViewMode(viewMode);
+        mMapController.setMapViewTrafficMode(trafficMode);
+        mMapController.setMapViewBicycleMode(bicycleMode);
+        mMapController.setMapCenter(new NGeoPoint(longitudeE6, latitudeE6), level);
+
+        if (mIsMapEnlared) {
+            mMapView.setScalingFactor(2.0F);
+        } else {
+            mMapView.setScalingFactor(1.0F);
+        }
+    }
+
+    // 터치 이벤트 리스너 정의
+    private final NMapView.OnMapViewTouchEventListener onMapViewTouchEventListener = new NMapView.OnMapViewTouchEventListener() {
+
+        @Override
+        public void onLongPress(NMapView mapView, MotionEvent ev) {
+            // TODO Auto-generated method stub
+        }
+
+        @Override
+        public void onLongPressCanceled(NMapView mapView) {
+            // TODO Auto-generated method stub
+        }
+
+        @Override
+        public void onSingleTapUp(NMapView mapView, MotionEvent ev) {
+            // TODO Auto-generated method stub
+        }
+
+        @Override
+        public void onTouchDown(NMapView mapView, MotionEvent ev) {
+        }
+
+        @Override
+        public void onScroll(NMapView mapView, MotionEvent e1, MotionEvent e2) {
+        }
+
+        @Override
+        public void onTouchUp(NMapView mapView, MotionEvent ev) {
+            // TODO Auto-generated method stub
+        }
+    };
 }
+
